57bae4110469cf0ab518463092964133989b6311
Christian Jonassen <flyrev@gmail.com>
2016-06-29 11:39:58 +0200

    .emacs: markdown

--- .emacs
+++ .emacs
@@ -40,3 +40,8 @@
 (add-hook 'web-mode-hook (lambda () (ember-mode t)))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
--- setup-emacs.sh
+++ setup-emacs.sh
@@ -1,5 +1,9 @@
 cp .emacs ~/
 mkdir -p ~/.emacs.d/modes
-wget https://raw.github.com/fxbois/web-mode/master/web-mode.el 
+wget https://raw.github.com/fxbois/web-mode/master/web-mode.el
 wget https://raw.githubusercontent.com/madnificent/ember-mode/master/ember-mode.el
 mv -v *.el ~/.emacs.d/modes
+
+echo Now do
+echo M-x package-install RET markdown-mode RET
+echo And you are good to go
